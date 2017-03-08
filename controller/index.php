6bff1a30e9775527e8f9c3a03ede1f76b0695a7c
criscros22 <hjudge.singh@gmail.com>
2017-03-08 15:09:34 -0500

    added index and view

---
+++ controller/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="../view/style.css">
+        <title> Finesse Co. </title>
+    </head>
+    <body>
+        <div class="header">
+            <div class="links">
+                <a href="login.php">Login</a>
+                <a href="register.php">Register Now</a>
+            </div>
+            <div class="title">
+                <h1>Finesse Co.</h1>
+                <h3>Buy and Sell</h3>
+            </div>
+            <div class="categories">
+                <a href="laptops.php">Laptops</a>
+                <a href="tablets.php">Tablets</a>
+                <a href="phones.php">Phones</a>
+                <a href="tv.php">TVs</a>
+                <a href="mp3.php">MP3s</a>
+            </div>
+        </div>
+    </body>
+
+</html>

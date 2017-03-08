e8813d40d0f649ceefe3b855c8f59e7bce69f079
criscros22 <hjudge.singh@gmail.com>
2017-03-08 15:15:14 -0500

    added login and register files

---
+++ view/login.php
@@ -0,0 +1,20 @@
+<?php
+// So I don't have to deal with unset $_REQUEST['user'] when refilling the form
+$_REQUEST['user']=!empty($_REQUEST['user']) ? $_REQUEST['user'] : '';
+$_REQUEST['password']=!empty($_REQUEST['password']) ? $_REQUEST['password'] : '';
+?>
+<!DOCTYPE HTML>
+<html>
+	<h2>Login</h2>
+	<form>
+		<p class="userpass">
+			<label for="user">User: </label>
+			<input type="text" id="user"></input>
+		</p>
+		<p class="userpass">
+			<label for="password">Password: </label>
+			<input type="password" id="password"></input>
+		</p>
+		<input type="submit"> Submit </input>
+	</form>
+</html>
---
+++ view/register.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>
+<h1>Register</h1>
+<form>
+<p>
+    <label for="ruser">User: </label>
+    <input type="text" id="ruser"></input>
+</p>
+<p>
+    <label for="rpassword">Password: </label>
+    <input type="password" id="rpassword"></input>
+</p>
+<input type="submit" name="Submit" value="submit"> Submit </input>
+</form>
+</div>
+</html>

00fc2b4093a391e8a0d7784581ba3fd202444608
Osama Alam <osama.alam@mail.utoronto.ca>
2017-03-08 21:33:28 -0500

    Demo1 + reAdded register

--- view/register.php
+++ view/register.php
@@ -1,16 +1,44 @@
 <!DOCTYPE HTML>
 <html>
 <h1>Register</h1>
-<form action="login.php">
+<form>
 <p>
     <label for="ruser">User: </label>
-    <input type="text" id="ruser"></input>
+    <input type="text" id="ruser">
 </p>
 <p>
     <label for="rpassword">Password: </label>
-    <input type="password" id="rpassword"></input>
+    <input type="password" id="rpassword">
 </p>
-<input type="submit" name="Submit" value="submit"> Submit </input>
+<p>
+    <label for="fName"> First Name </label>
+    <input type="text" name="fName">
+</p>
+<p>
+    <label for="lName">Last Name</label>
+    <input type="text" name="lName">
+</p>
+<p>
+    <label for="rcountry"> Country </label>
+    <input type="text" name="rCountry">
+</p>
+<p>
+    <label for "rEmail">E-mail</label>
+    <input type="email" name="rEmail">
+</p>
+<p>
+    <label for="state"> Pronvince/State </label>
+    <input type="text" name="rState">
+</p>
+<p>
+    <label for="city"> Street Number and Address:</label>
+    <input type="text" name="rCity">
+</p>
+<p>
+    <label for="street">Street Name and Number</label>
+    <input type="text" name="rStreet">
+</p>
+    <input type="submit" name="Submit" value="submit"> Submit </input>
 </form>
 </div>
 </html>
